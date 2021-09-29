c18f7fd5d4eda0f9c3687bf15ed31571e17e33b0
bw2 <weisburd@broadinistitute.org>
2021-09-28 23:38:26 -0400

    updated table header

--- generate_website.sh
+++ generate_website.sh
@@ -2,6 +2,6 @@ rm -rf docs
 python3 write_out_metadata_files.py
 
 cd images
-python3 -m flipbook --generate-static-website -s Locus -s EH_long -s EH_short
+python3 -m flipbook --generate-static-website -s "Locus_(Inheritance)" -s "EH_long" -s "EH_short"
 mv flipbook_html ../docs
 echo "Moved website to docs dir"
