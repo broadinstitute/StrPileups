b28e3985a715e64241c2533b82a57ba93c437a09
bw2 <weisburd@broadinistitute.org>
2021-09-28 23:06:09 -0400

    updated table and regenerated using flipbook v0.11.1

--- generate_website.sh
+++ generate_website.sh
@@ -1,4 +1,7 @@
 rm -rf docs
-python3 -m flipbook --generate-static-website
-mv flipbook_html docs
+python3 write_out_metadata_files.py
+
+cd images
+python3 -m flipbook --generate-static-website -s Locus -s EH_long -s EH_short
+mv flipbook_html ../docs
 echo "Moved website to docs dir"
